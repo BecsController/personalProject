1d0f4e5804c1db67914e24155649b0f49b73a181
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-07-04 20:02:48 +1200

    reorganise user page

--- client/components/users/UserPage.jsx
+++ client/components/users/UserPage.jsx
@@ -21,50 +21,45 @@ class User extends React.Component {
 
   render() {
     return (
-      <div className="column is-10 is-offset-1">
-        <div className="columns box is-multiline" id="grid">
-          <div key={this.state.user.name} className="column is-3 is-3-widescreen is-flex">
+      <div className="column has-text-centered is-10 is-offset-1">
+          <h3 className="box is-size-1 has-text-grey-dark">{this.state.user.name}</h3>
 
-            <div className="card is-flex" style={{height: '20vw'}}>
-              <div style={{width: '100%'}} className="card-title title is-3">
-                <h3 className="has-text-grey-dark">{this.state.user.name}</h3>
-              </div>
+        <div className="box is-multiline is-flex" id="grid">
 
-              <div style={{marginTop: '5vw', width: '30vw', height: '15vw'}} className="box card-image">
+              <div style={{width: '20vw', height: '25vw'}} className="box card-image">
                 <figure className="image is-1by1">
                   <img src={this.state.user.avatar} alt={this.state.user.name}/>
                 </figure>
               </div>
-            </div>
-            <div className="box">
+
+            <div style={{width: '40vw'}} className="box">
               <h4 className="has-text-grey-dark">Email: {this.state.user.email}</h4>
               <h4 className="has-text-grey-dark">Stories: {this.state.user.saved_stories}</h4>
-            {!this.state.user.saved_avatar &&<Link to={`/user/${this.state.user.id}/createAvatar`}><button className="is-rounded button is-medium">Create Avatar</button>
-        </Link>}
-            </div>
-            {this.state.user.saved_avatar && <div className="box card-image">
-              <figure className="image is -1by-1">
-                <img src={this.state.user.saved_avatar} alt={this.state.user.name} />
-              </figure>
-              <Link className="is-rounded button is-medium" to={`/user/${this.state.user.id}/editAvatar`}>
-                Edit Avatar
-              </Link>
-            </div>}
-            {this.state.user.saved_stories && <div className="box card-image">
-              <ul>
-                {this.state.user.saved_stories.map(story => {
-                  return (
-                    <li>story</li>
-                  )
-                })}
-              </ul>
-            </div>}
-          </div>
+              {!this.state.user.saved_avatar &&<Link to={`/user/${this.state.user.id}/createAvatar`}><button style={{marginTop: '3vw'}} className="is-rounded button is-medium">Create Avatar</button>
+          </Link>}
         </div>
-        <Link to={`/users`}><button className="button is-medium is-rounded is-link">Back To Users Page</button></Link>
-      </div>
-    )
-  }
+        {this.state.user.saved_avatar && <div style={{width: '20vw', height: '25vw'}} className="box card-image">
+          <figure className="image is -1by-1">
+            <img src={this.state.user.saved_avatar} alt={this.state.user.name} />
+          </figure>
+          <Link style={{marginTop: '3vw'}} className="is-rounded button is-medium" to={`/user/${this.state.user.id}/editAvatar`}>
+            Edit Avatar
+          </Link>
+        </div>}
+        {this.state.user.saved_stories && <div className="box card-image">
+          <ul>
+            {this.state.user.saved_stories.map(story => {
+              return (
+                <li>story</li>
+              )
+            })}
+          </ul>
+        </div>}
+    </div>
+    <Link to={`/users`}><button className="button is-medium is-rounded is-link">Back To Users Page</button></Link>
+  </div>
+)
+}
 }
 
 export default User
