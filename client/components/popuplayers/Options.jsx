57f4d93597527b7b185c7af85422df8fa1c32916
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-08-08 22:12:42 +1200

    attempting to add redux to options component needs fixing

--- client/components/popuplayers/Options.jsx
+++ client/components/popuplayers/Options.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-export default function Options ({page, tracker}) {
-
-  return (
-    <div>
-      <button className="button has-text-centered is-rounded is-medium is-size-4 is-info is-pulled-left">
-        {page[tracker].optionOne}
-      </button>
-      <button className="button has-text-centered is-rounded is-medium is-size-4 is-info is-pulled-right">
-        {page[tracker].optionTwo}
-      </button>
-    </div>
-  )
+class Options extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+    pages: [],
+  }
 }
+
+componentDidMount () {
+  this.props.dispatch(getPages())
+}
+
+componentWillReceiveProps(nextProps){
+  this.setState({
+    pages: nextProps.pages,
+  })
+}
+
+render () {
+  console.log(this.state)
+    return (
+      <div>
+        <button className="button has-text-centered is-rounded is-medium is-size-4 is-info is-pulled-left">
+          {this.state.pages[0]}
+        </button>
+        <button className="button has-text-centered is-rounded is-medium is-size-4 is-info is-pulled-right">
+          {this.state.pages[0]}
+        </button>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps =  (pages) => {
+  return { pages }
+}
+export default connect(mapStateToProps)(Options)
--- client/components/story/StoryPage.jsx
+++ client/components/story/StoryPage.jsx
@@ -19,7 +19,6 @@ class StoriesPage extends React.Component {
     }
     this.showAnswerOptions = this.showAnswerOptions.bind(this)
     this.showEmotionButtons = this.showEmotionButtons.bind(this)
-    this.chooseAnswer = this.chooseAnswer.bind(this)
   }
 
   componentDidMount () {
@@ -39,12 +38,8 @@ class StoriesPage extends React.Component {
     })
   }
 
-  chooseAnswer () {
-
-  }
-
   showEmotionButtons () {
-  let emotionState = this.state.buttonsVisible ? false : true
+    let emotionState = this.state.buttonsVisible ? false : true
     this.setState ({
       buttonsVisible: emotionState
     })
@@ -62,27 +57,25 @@ class StoriesPage extends React.Component {
           {this.state.buttonsVisible && <EmotionButtons page={this.state.pages} tracker={this.state.pageTracker}/>}
         </div>
 
-          {this.state.pages.length > 0 && <div className="column is-10 box">
-            <h1 className="has-text-centered is-size-2">{this.state.pages[this.state.pageTracker].title}</h1>
-            <img className="background-img" src={this.state.pages[this.state.pageTracker].background}/>
+        {this.state.pages.length > 0 && <div className="column is-10 box">
+          <h1 className="has-text-centered is-size-2">{this.state.pages[this.state.pageTracker].title}</h1>
+          <img className="background-img" src={this.state.pages[this.state.pageTracker].background}/>
+          <img className="population-img" src={this.state.pages[this.state.pageTracker].population} alt="people"/>
 
-
-              <img className="population-img" src={this.state.pages[this.state.pageTracker].population} alt="people"/>
-
-              <div className="textarea box">
-                <p className="has-text-centered is-size-4">
-                  {this.state.pages[this.state.pageTracker].pageText}
-                  {this.state.selectorVisible && <Link className="button is-medium is-pulled-right" to={`#`} onClick={this.showAnswerOptions}>
-                    Click to answer &nbsp;
-                    <span className="icon"><i className="far fa-arrow-alt-circle-right"></i></span>
-                  </Link>}
-                {this.state.optionsVisible && <Options page={this.state.pages} tracker={this.state.pageTracker}/>}
-                </p>
-</div>
-          </div>}
-      </div>
-    )
-  }
+          <div className="textarea box">
+            <p className="has-text-centered is-size-4">
+              {this.state.pages[this.state.pageTracker].pageText}
+              {this.state.selectorVisible && <Link className="button is-medium is-pulled-right" to={`#`} onClick={this.showAnswerOptions}>
+              Click to answer &nbsp;
+              <span className="icon"><i className="far fa-arrow-alt-circle-right"></i></span>
+            </Link>}
+            {this.state.optionsVisible && <Options page={this.state.pageTracker}/>}
+          </p>
+        </div>
+      </div>}
+    </div>
+  )
+}
 }
 
 const mapStateToProps = state => state
