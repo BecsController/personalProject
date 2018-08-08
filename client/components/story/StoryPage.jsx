77309ea716b93ba77a014df8ac39223ca19f6607
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-08-08 20:22:25 +1200

    beginnings of page styling changes

--- client/components/story/StoryPage.jsx
+++ client/components/story/StoryPage.jsx
@@ -50,22 +50,24 @@ class StoriesPage extends React.Component {
   render() {
     return (
       <div className="columns is-two-thirds">
+
         <div className="column is-4 box">
           <Link className="button is-medium" to={`#`} onClick={this.showEmotionButtons}>
             How are you feeling?
           </Link>
-          
+          <hr />
+          {this.state.buttonsVisible && <EmotionButtons page={this.state.pages} tracker={this.state.pageTracker}/>}
         </div>
+
         <div className="column is-8">
 
           {this.state.pages.length > 0 && <div className="column is-10 is-offset-1 page-template box">
             <h1 className="has-text-centered is-size-2">{this.state.pages[this.state.pageTracker].title}</h1>
-            <img style={{width: '50vw', position: 'absolute', zIndex: '0', top: '5vw', left: '20vw'}} src={this.state.pages[this.state.pageTracker].background}/>
+            <img className="background-img" src={this.state.pages[this.state.pageTracker].background}/>
 
             <div className="container is-full-height ">
               <img style={{width: '20vw', position: 'absolute', top: '-20vw', left: '-2vw', zIndex: '3'}} src={this.state.pages[this.state.pageTracker].population} alt="teacher"/>
 
-              {this.state.buttonsVisible && <EmotionButtons page={this.state.pages} tracker={this.state.pageTracker}/>}
               <div className="box column is-10 textarea">
                 <p className="has-text-centered is-size-4">
                   {this.state.pages[this.state.pageTracker].pageText}
