daa96256e472aad37830c648bf75765b5e4b230b
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-08-28 17:37:52 +1200

    separated out routes test for clarity

---
+++ test/server/associations.routes.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest')
+
+jest.mock('../../server/db/associations', () => ({
+  getAssociations: () => Promise.resolve([
+    {id: 1, page_id: 1, emotion: 'anxious', user_id: 5},
+    {id: 2, page_id: 2, emotion: 'excited', user_id: 5},
+    {id: 3, page_id: 3, emotion: 'scared', user_id: 5}
+  ]),
+  createAssociations: () => Promise.resolve([
+    {id: 4, page_id: 4, emotion: 'angry', user_id: 7}
+  ])
+}))
+
+const server = require('../../server/server')
+
+test('Get all associations', () => {
+  return request(server)
+    .get('/api/associations')
+    .expect(200)
+    .then(res => {
+      expect(res.body.length).toBe(3)
+      expect(res.body[0].emotion).toBe('anxious')
+      expect(res.body[1].user_id).toBe(5)
+      expect(res.body[1].id).toBe(2)
+    })
+    .catch(err => {
+      expect(err).toBeFalsy()
+    })
+})
--- test/server/user.routes.test.js
+++ test/server/user.routes.test.js
@@ -20,21 +20,6 @@ jest.mock('../../server/db/user', () => ({
   ])
 }))
 
-jest.mock('../../server/db/stories', () => ({
-  getStories: () => Promise.resolve([
-    {id: 1, title: 'School'},
-    {id: 2, title: 'The Mall'},
-    {id: 3, title: 'Wash hands'}
-  ]),
-  getStory: () => Promise.resolve([
-    {id: 2, title: 'Wash hands', user_id: 2, genre: 'hygiene'}
-  ]),
-  getPages: () => Promise.resolve([
-    {id: 1, background: 'chair', population: 'parents'},
-    {id: 2, background: 'classroom', population: 'teacher'}
-  ])
-}))
-
 const server = require('../../server/server')
 
 test('Get all users', () => {
@@ -108,50 +93,4 @@ test('create a user', () => {
     .catch(err => {
       expect(err).toBeFalsy()
     })
-})
-
-test('Get all stories', () => {
-  return request(server)
-    .get('/api/stories')
-    .expect(200)
-    .then(res => {
-      expect(res.body.stories.length).toBe(3)
-      expect(res.body.stories[2].title).toBe('Wash hands')
-      expect(res.body.stories[1]).toHaveProperty('title')
-    })
-    .catch(err => {
-      expect(err).toBeFalsy()
-    })
-})
-
-test('Get single story', () => {
-  const id = 2
-  return request(server)
-    .get('/api/stories/:id')
-    .expect(200)
-    .then(res => {
-      expect(res.body[0].title).toBe('Wash hands')
-      expect(res.body.length).toBe(1)
-      expect(res.body[0].id).toBe(2)
-      expect(res.body[0].genre).toBe('hygiene')
-      expect(res.body[0].user_id).toBe(2)
-    })
-    .catch(err => {
-      expect(err).toBeFalsy()
-    })
-})
-
-test('Get pages returns all pages', () => {
-  return request(server)
-    .get('/api/pages')
-    .expect(200)
-    .then(res => {
-      expect(res.body.pages.length).toBe(2)
-      expect(res.body.pages[0].background).toBe('chair')
-      expect(res.body.pages[1].population).toBe('teacher')
-      expect(res.body.pages[1].id).toBe(2)
-  })
-  .catch(err => {
-    expect(err).toBeFalsy()
-  })
 })
