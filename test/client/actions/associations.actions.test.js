37308363c7e47cd4ad54c47ae13533309f341af6
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-08-30 15:19:09 +1200

    first association action tests

---
+++ test/client/actions/associations.actions.test.js
@@ -0,0 +1,59 @@
+import {
+  getAssociations,
+  newEmotion,
+  addAssociation,
+  receiveAssociations
+} from '../../../client/actions/associations'
+import nock from 'nock'
+
+test('Receive associations action creator', () => {
+  const fakeAssociations = [
+    'Angry',
+    'Sad',
+    'Nervous',
+    'Excited'
+  ]
+
+  const expected = {
+    type: 'RECEIVE_ASSOCIATIONS',
+    associations: fakeAssociations
+  }
+
+  const actual = receiveAssociations(fakeAssociations)
+
+  expect(actual.associations.length).toBe(4)
+  expect(actual).toEqual(expected)
+})
+
+test('getAssociations will dispatch an action on success', () => {
+  const fakeAssociations = [
+    'Happy',
+    'Scared'
+  ]
+  const scope = nock('http://localhost:80')
+    .get('/api/associations')
+    .reply(200, fakeAssociations);
+
+  const expectedAction = {
+    type: 'RECEIVE_ASSOCIATIONS',
+    associations: fakeAssociations
+  }
+
+  const dispatch = jest.fn()
+    .mockImplementationOnce(action => {
+      expect(action).toEqual(expectedAction)
+      scope.done()
+    })
+
+  getAssociations()(dispatch)
+
+})
+
+test('getAssociations error', () => {
+  const scope = nock('http://localhost:80')
+    .get('/api/associations')
+    .reply(404);
+
+  const actual = getAssociations()()
+  expect(actual).toBe(undefined)
+})
