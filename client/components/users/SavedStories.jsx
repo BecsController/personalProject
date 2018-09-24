5addecd7eff66c8868c6ec2c45aea3beef309bc0
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-09-24 13:35:57 +1200

    parsing json strings back to strings

--- client/components/users/SavedStories.jsx
+++ client/components/users/SavedStories.jsx
@@ -29,8 +29,12 @@ class SavedStories extends React.Component {
   }
 
   displayStoryInfo(story_id){
+    let current = this.state.currentStory.find(story => story.id == story_id)
+      current.answers = JSON.parse(current.answers)
+      current.questions = JSON.parse(current.questions)
+      current.emotions = JSON.parse(current.emotions)
     this.setState({
-      clickedStory: this.state.currentStory.find(story => story.id == story_id),
+      clickedStory: current,
       displayCurrent: true
     })
   }
@@ -41,9 +45,9 @@ class SavedStories extends React.Component {
         <p className="is-size-3 has-text-centered box">Click A Saved Story's Title To See More Information</p>
          <div className="box" style={{paddingBottom: '30vw', marginLeft: '.3vw', marginRight: '.3vw'}}>
           <div className="column is-multiline">
-            {this.state.currentStory && this.state.currentStory.map(story => {
+            {this.state.currentStory && this.state.currentStory.map((story, i) => {
               return (
-                <div className="columns is-4 is-flex">
+                <div key={i} className="columns is-4 is-flex">
                   <a onClick={() => this.displayStoryInfo(story.id)}>{story.title}</a>
                 </div>
               )
